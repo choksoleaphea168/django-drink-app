7566161142fc87b2e60adab7a453a09dafe146de
CHOK VEASNA <veasna.chok@CAB.COM.KH>
2026-09-06 15:15:13 +0700

    Fix static files distDir path and WhiteNoise storage in vercel.json & settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -135,7 +135,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 
 # Email
